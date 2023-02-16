575c9d92c292c60cd3e133868abedc392b981bb8
muhammadridhwan10 <81613685+muhammadridhwan10@users.noreply.github.com>
2023-02-16 17:00:34 +0700

    update bahasa attendance

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -144,7 +144,7 @@
                     <div class="col-xxl-6">
                         <div class="card">
                             <div class="card-header">
-                                <h4>{{__('Mark Attandance')}}</h4>
+                                <h4>{{__('Mark Attendance')}}</h4>
                             </div>
                             <div class="card-body dash-card-body">
                                 <p class="text-muted pb-0-5">{{__('My Office Time: '.$officeTime['startTime'].' to '.$officeTime['endTime'])}}</p>
@@ -269,7 +269,7 @@
             <div class="col-xxl-12">
             <div class="card">
                             <div class="card-header">
-                                <h4>{{__('Mark Attandance')}}</h4>
+                                <h4>{{__('Mark Attendance')}}</h4>
                             </div>
                             <div class="card-body dash-card-body">
                                 <p class="text-muted pb-0-5">{{__('My Office Time: '.$officeTime['startTime'].' to '.$officeTime['endTime'])}}</p>
